9562f22f1eccab759a8bbab770760e7eb6cf9733
franciscoabsampaio <franciscoabsampaio@proton.me>
2025-10-08 22:08:14 +0100

    Add: test comment

--- spark-connect-sql/src/test_utils.rs
+++ spark-connect-sql/src/test_utils.rs
@@ -3,6 +3,7 @@ pub mod test_utils {
     use crate::{SparkSession, SparkSessionBuilder, error::SparkError};
 
     /// Test fixture to create a SparkSession.
+    /// Requires a Spark Connect server instance available at localhost:15002.
     pub async fn setup_session() -> Result<SparkSession, SparkError> {
         let connection = format!("sc://localhost:15002");
 
