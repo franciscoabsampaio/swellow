c5a4c7ccbd7a0047682ab74c4c36f4099be60984
franciscoabsampaio <franciscoabsampaio@proton.me>
2025-10-19 15:54:26 +0100

    Remove: spark-connect-server Dockerfile (moved to separate repo)

--- tests/spark-delta/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Start Spark Connect Server
-/opt/spark/bin/spark-submit \
-    --class org.apache.spark.sql.connect.service.SparkConnectServer \
-    --conf spark.connect.grpc.binding=0.0.0.0:15002 \
-    --conf spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension \
-    --conf spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog \
-    --jars /opt/spark/jars/delta-spark_2.12-3.2.0.jar,/opt/spark/jars/delta-storage-3.2.0.jar,/opt/spark/jars/spark-connect_2.12-3.5.7.jar \
-    --name spark-connect-server
